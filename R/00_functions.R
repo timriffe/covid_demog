4e4cacb6ec0aebbefc463e18a87740d855b29a25
Tim Riffe <tim.riffe@gmail.com>
2020-03-25 23:11:27 +0100

    first pass at data standardization, using pclm. Right now doing 10-year age groups, but could switch to 5. Some things are hard coded, facepalm

--- R/00_functions.R
+++ R/00_functions.R
@@ -6,14 +6,56 @@
 library(here)
 library(tidyverse)
 library(ungroup)
-
+library(DemoTools)
 # 
 
-
+# --------------------------------
 # redistribute unknown age cases and deaths, tidy
-
+# designed for tidy pipeline. Changes number of rows,
+# so do it inside do()
+redistribute_NAs <- function(.chunk){
+  if (any(.chunk$Age == "UNK")){
+    UNK <- .chunk %>% filter(Age == "UNK")
+    KN  <- .chunk %>% filter(Age != "UNK")
+    .chunk <- 
+      KN %>% mutate(Cases = Cases + (Cases / sum(Cases)) * UNK$Cases,
+                    Deaths = Deaths + (Deaths / sum(Deaths)) * UNK$Deaths)
+  }
+  .chunk
+}
+# --------------------------------
 # standardize age groups cases and deaths, using pclm
-
+# designed for tidy pipeline. Changes number of rows,
+# so do it inside do()
+standardize_chunk <- function(.chunk, N = 10){
+  
+  n       <- nrow(.chunk)
+  nlast   <- .chunk %>% pull(AgeInt) %>% "[["(n)
+  x       <- .chunk %>% pull(Age) %>% unlist()
+  y_cases <- .chunk %>% pull(Cases) %>% unlist()
+  y_deaths <- .chunk %>% pull(Deaths) %>% unlist()
+  offset  <- pclm(x = x, 
+                  y = y_cases, 
+                  nlast = nlast,
+                  control = list(lambda = 100))$fitted
+  
+  # age-specific case fataity rates, single ages.
+  ascfr  <- pclm(x = x,
+                 y = y_deaths,
+                 offset = offset,
+                 nlast = nlast,
+                 control = list(lambda = 100))$fitted
+  deaths <- ascfr * offset
+  # sry hard coded for now
+  x1     <- 0:104
+  
+  # group deaths and cases back up to N-year age groups
+  Dhat   <- groupAges(deaths, Age = x1, N = N, OAnew = 100)
+  Chat   <- groupAges(offset, Age = x1, N = N, OAnew = 100)
+  Age    <- names2age(Dhat)
+  AgeInt <- age2int(Age,OAvalue = 5)
+  tibble(Age = Age, AgeInt = AgeInt, Cases = Chat, Deaths = Dhat)
+}
 
 
 # cfr CD's version
--- R/02_data_prep.R
+++ R/02_data_prep.R
@@ -3,40 +3,26 @@
 
 # creates object 'dat'
 source(here("R/01_input_data.R"))
+source(here("R/00_functions.R"))
 
-head(dat)
 
 # first, let's standardize age groups of deaths and cases.
 # to 5 or 10-year age groups 0-100.
 
 # 1) redistribute unknown cases and deaths.
 
-redistribute_NAs <- function(.chunk){
-  if (any(.chunk$Age == "UNK")){
-  UNK <- .chunk %>% filter(Age == "UNK")
-  KN  <- .chunk %>% filter(Age != "UNK")
-  .chunk <- 
-    KN %>% mutate(Cases = Cases + (Cases / sum(Cases)) * UNK$Cases,
-                  Deaths = Deaths + (Deaths / sum(Deaths)) * UNK$Deaths)
-  }
-  .chunk
-}
 
-.data <- dat %>% 
-  filter(Country == "Spain",
-         Sex == "b",
-         Date == "24.03.2020")
+# TR: Q: I'
+dat <- dat %>% 
+  filter(Country != "Canada") %>% 
+  group_by(Country, Date, Code, Sex) %>% 
+  do(redistribute_NAs(.chunk = .data)) %>% 
+  do(standardize_chunk(.chunk = .data)) %>% 
+  unnest(cols = c())
 
-standardize_chunk <- function(.chunk, N = 10){
-  n <- nrow(.chunk)
-  pclm(x = .chunk$Age, 
-       y = .chunk$Cases, 
-       nlast = .chunk$AgeInt[n])
-}
+  
 
 
-dat %>% 
-  nest(Code, Sex) %>% 
-  do(redistribute_NAs(.chunk = .data))
+
   
 
