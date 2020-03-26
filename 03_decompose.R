f72db70c02c93fd0525306139cf951c639b39d1c
Tim Riffe <tim.riffe@gmail.com>
2020-03-26 23:37:00 +0100

    comment out ggplot of Italy

--- 03_decompose.R
+++ 03_decompose.R
@@ -8,14 +8,16 @@ ST <- dat %>%
   filter(Code == "CN11.02.2020")
 
 
-dat %>% 
-  filter(Country == "Italy",
-         Sex == "f") %>% 
-  # doing the David Spiegelhalter trick of plotting on 7s
-  ggplot(mapping = aes(x = Age+7, y = ascfr, color = Date))+
-  geom_line() +
-  scale_y_log10() + 
-  ylim(1e-4,.35) +
-  xlim(25,97)
+# dat %>% 
+#   filter(Country == "Italy",
+#          Sex == "b") %>% 
+#   # doing the David Spiegelhalter trick of plotting on 7s
+#   ggplot(mapping = aes(x = Age+7, y = ascfr, color = Date))+
+#   geom_line() +
+#   scale_y_log10() + 
+#   ylim(1e-4,.3) +
+#   xlim(25,97) + 
+#   geom_line(ST,mapping=aes(x=Age+7, y=ascfr),color="black") + 
+#   geom_lin(filter(dat,Country = "SouthKorea"))
   
 
