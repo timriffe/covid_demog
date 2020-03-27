e474a341c460fba28b3d055eea8428e2e2319294
Tim Riffe <tim.riffe@gmail.com>
2020-03-27 07:29:03 +0100

    add France source

--- 03_decompose.R
+++ 03_decompose.R
@@ -5,19 +5,23 @@ source("R/02_data_prep.R")
 # pick a standard
 
 ST <- dat %>% 
-  filter(Code == "CN11.02.2020")
+  filter(Country == "China",
+         Sex == "b")
 
 
-# dat %>% 
-#   filter(Country == "Italy",
-#          Sex == "b") %>% 
-#   # doing the David Spiegelhalter trick of plotting on 7s
-#   ggplot(mapping = aes(x = Age+7, y = ascfr, color = Date))+
-#   geom_line() +
-#   scale_y_log10() + 
-#   ylim(1e-4,.3) +
-#   xlim(25,97) + 
-#   geom_line(ST,mapping=aes(x=Age+7, y=ascfr),color="black") + 
-#   geom_lin(filter(dat,Country = "SouthKorea"))
+ dat %>% 
+   filter(Country == "Italy",
+          Sex == "b") %>% 
+   # doing the David Spiegelhalter trick of plotting on 7s
+   ggplot(mapping = aes(x = Age+7, y = ascfr, color = Date, by = as.factor(Date)))+
+   geom_line() +
+   scale_y_log10() + 
+   ylim(1e-4,.3) +
+   xlim(25,97) + 
+   geom_line(ST,mapping=aes(x=Age+7, y=ascfr),color="black",size=2) + 
+   geom_line(filter(dat,
+                    Code == "SK26.03.2020",
+                    Sex == "b"),
+             mapping=aes(x=Age+7, y=ascfr), color = "red",size=2)
   
 
